a8159618f61dea777197b8560e8e92b8fbaf3390
Simon <sstur@me.com>
2023-02-01 15:58:08 -0800

    Setup for task2

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
-/* eslint-disable react/style-prop-object */
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useState } from 'react';
+import { SafeAreaView, View, Text, Button } from 'react-native';
+type Task = {
+  id: number;
+  name: string;
+};
+const initialTasks: Array<Task> = [
+  { id: 1, name: 'Make coffee' },
+  { id: 2, name: 'Do Laundry' },
+];
 export function App() {
+  const [taskItems, setTaskItems] = useState(initialTasks);
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ padding: 20 }}>
+        <Text>Tasks:</Text>
+        {/* TODO: loop through the tasks and for each one display task.title  */}
+        <Button
+          title="Add new task"
+          onPress={() => {
+            // TODO
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
